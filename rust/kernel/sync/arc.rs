f8dc16e2672243b6e5ba693bf5678e251bb794bf
Wedson Almeida Filho <wedsonaf@gmail.com>
2022-12-28 06:03:42 +0000

    rust: sync: allow coercion from `Arc<T>` to `Arc<U>`
    The coercion is only allowed if `U` is a compatible dynamically-sized
    type (DST). For example, if we have some type `X` that implements trait
    `Y`, then this allows `Arc<X>` to be coerced into `Arc<dyn Y>`.
    
    Suggested-by: Gary Guo <gary@garyguo.net>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
    Acked-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/sync/arc.rs
+++ rust/kernel/sync/arc.rs
@@ -129,6 +129,10 @@ struct ArcInner<T: ?Sized> {
 // This is to allow [`Arc`] (and variants) to be used as the type of `self`.
 impl<T: ?Sized> core::ops::Receiver for Arc<T> {}
 
+// This is to allow coercion from `Arc<T>` to `Arc<U>` if `T` can be converted to the
+// dynamically-sized type (DST) `U`.
+impl<T: ?Sized + Unsize<U>, U: ?Sized> core::ops::CoerceUnsized<Arc<U>> for Arc<T> {}
+
 // This is to allow [`Arc`] (and variants) to be used as the type of `self`.
 impl<T: ?Sized> core::ops::Receiver for Arc<T> {}
 
