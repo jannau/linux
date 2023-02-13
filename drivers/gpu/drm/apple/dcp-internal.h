05cd8ead613dc1b61db6ce9ae98f5fc398867906
Martin Povišer <povik+lin@cutebit.org>
2023-02-13 14:56:24 +0100

    gpu: drm: apple: Set up client of AV endpoint
    Signed-off-by: Martin Povišer <povik+lin@cutebit.org>

--- drivers/gpu/drm/apple/dcp-internal.h
+++ drivers/gpu/drm/apple/dcp-internal.h
@@ -34,6 +34,7 @@ enum {
 	TEST_ENDPOINT = 0x21,
 	DCP_EXPERT_ENDPOINT = 0x22,
 	DISP0_ENDPOINT = 0x23,
+	AV_ENDPOINT = 0x29,
 	DPTX_ENDPOINT = 0x2a,
 	HDCP_ENDPOINT = 0x2b,
 	REMOTE_ALLOC_ENDPOINT = 0x2d,
@@ -87,6 +88,8 @@ struct dcp_brightness {
 	int scale;
 	bool update;
 };
+
+struct audiosrv_data;
 
 /** laptop/AiO integrated panel parameters from DT */
 struct dcp_panel {
@@ -218,6 +221,9 @@ struct apple_dcp {
 	struct completion systemep_done;
 
 	struct apple_dcp_afkep *ibootep;
+
+	struct apple_dcp_afkep *avep;
+	struct audiosrv_data *audiosrv;
 
 	struct apple_dcp_afkep *dptxep;
 
