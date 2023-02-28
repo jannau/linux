c2daca54edb1c7497282319067e22022cd9ed7fc
Janne Grunau <j@jannau.net>
2023-02-28 21:37:52 +0100

    drm/apple: Support DCP FW version 13.2
    So far only tested on Mac mini (M1, 2020, j274).
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/iomfb_template.c
+++ drivers/gpu/drm/apple/iomfb_template.c
@@ -79,7 +79,11 @@ DCP_THUNK_INOUT(dcp_set_display_device, dcpep_set_display_device, u32, u32);
 DCP_THUNK_OUT(dcp_set_display_refresh_properties,
 	      dcpep_set_display_refresh_properties, u32);
 
+#if DCP_FW_VER >= DCP_FW_VERSION(13, 2, 0)
+DCP_THUNK_INOUT(dcp_late_init_signal, dcpep_late_init_signal, u32, u32);
+#else
 DCP_THUNK_OUT(dcp_late_init_signal, dcpep_late_init_signal, u32);
+#endif
 DCP_THUNK_IN(dcp_flush_supports_power, dcpep_flush_supports_power, u32);
 DCP_THUNK_OUT(dcp_create_default_fb, dcpep_create_default_fb, u32);
 DCP_THUNK_OUT(dcp_start_signal, dcpep_start_signal, u32);
@@ -434,18 +438,18 @@ static u64 dcpep_cb_get_frequency(struct apple_dcp *dcp)
 	return clk_get_rate(dcp->clk);
 }
 
-static struct dcp_map_reg_resp dcpep_cb_map_reg(struct apple_dcp *dcp,
-						struct dcp_map_reg_req *req)
+static struct DCP_FW_NAME(dcp_map_reg_resp) dcpep_cb_map_reg(struct apple_dcp *dcp,
+						struct DCP_FW_NAME(dcp_map_reg_req) *req)
 {
 	if (req->index >= dcp->nr_disp_registers) {
 		dev_warn(dcp->dev, "attempted to read invalid reg index %u",
 			 req->index);
 
-		return (struct dcp_map_reg_resp){ .ret = 1 };
+		return (struct DCP_FW_NAME(dcp_map_reg_resp)){ .ret = 1 };
 	} else {
 		struct resource *rsrc = dcp->disp_registers[req->index];
 
-		return (struct dcp_map_reg_resp){
+		return (struct DCP_FW_NAME(dcp_map_reg_resp)){
 			.addr = rsrc->start, .length = resource_size(rsrc)
 		};
 	}
@@ -589,7 +593,12 @@ static void boot_5(struct apple_dcp *dcp, void *out, void *cookie)
 
 static void boot_4(struct apple_dcp *dcp, void *out, void *cookie)
 {
+#if DCP_FW_VER >= DCP_FW_VERSION(13, 2, 0)
+	u32 v_true = 1;
+	dcp_late_init_signal(dcp, false, &v_true, boot_5, NULL);
+#else
 	dcp_late_init_signal(dcp, false, boot_5, NULL);
+#endif
 }
 
 static void boot_3(struct apple_dcp *dcp, void *out, void *cookie)
@@ -733,7 +742,11 @@ static void dcp_on_set_parameter(struct apple_dcp *dcp, void *out, void *cookie)
 	struct dcp_set_parameter_dcp param = {
 		.param = 14,
 		.value = { 0 },
+#if DCP_FW_VER >= DCP_FW_VERSION(13, 2, 0)
+		.count = 3,
+#else
 		.count = 1,
+#endif
 	};
 	dev_dbg(dcp->dev, "%s", __func__);
 
@@ -979,8 +992,9 @@ TRAMPOLINE_INOUT(trampoline_map_physical, dcpep_cb_map_physical,
 		 struct dcp_map_physical_req, struct dcp_map_physical_resp);
 TRAMPOLINE_INOUT(trampoline_release_mem_desc, dcpep_cb_release_mem_desc, u32,
 		 u8);
-TRAMPOLINE_INOUT(trampoline_map_reg, dcpep_cb_map_reg, struct dcp_map_reg_req,
-		 struct dcp_map_reg_resp);
+TRAMPOLINE_INOUT(trampoline_map_reg, dcpep_cb_map_reg,
+		 struct DCP_FW_NAME(dcp_map_reg_req),
+		 struct DCP_FW_NAME(dcp_map_reg_resp));
 TRAMPOLINE_INOUT(trampoline_read_edt_data, dcpep_cb_read_edt_data,
 		 struct dcp_read_edt_data_req, struct dcp_read_edt_data_resp);
 TRAMPOLINE_INOUT(trampoline_prop_start, dcpep_cb_prop_start, u32, u8);
@@ -1000,66 +1014,6 @@ TRAMPOLINE_IN(trampoline_enable_backlight_message_ap_gated,
 	      iomfbep_cb_enable_backlight_message_ap_gated, u8);
 TRAMPOLINE_IN(trampoline_pr_publish, iomfb_cb_pr_publish,
 	      struct iomfb_property);
-
-static const iomfb_cb_handler cb_handlers[IOMFB_MAX_CB] = {
-	[0] = trampoline_true, /* did_boot_signal */
-	[1] = trampoline_true, /* did_power_on_signal */
-	[2] = trampoline_nop, /* will_power_off_signal */
-	[3] = trampoline_rt_bandwidth,
-	[100] = iomfbep_cb_match_pmu_service,
-	[101] = trampoline_zero, /* get_display_default_stride */
-	[102] = trampoline_nop, /* set_number_property */
-	[103] = trampoline_nop, /* set_boolean_property */
-	[106] = trampoline_nop, /* remove_property */
-	[107] = trampoline_true, /* create_provider_service */
-	[108] = trampoline_true, /* create_product_service */
-	[109] = trampoline_true, /* create_pmu_service */
-	[110] = trampoline_true, /* create_iomfb_service */
-	[111] = trampoline_true, /* create_backlight_service */
-	[116] = dcpep_cb_boot_1,
-	[117] = trampoline_false, /* is_dark_boot */
-	[118] = trampoline_false, /* is_dark_boot / is_waking_from_hibernate*/
-	[120] = trampoline_read_edt_data,
-	[122] = trampoline_prop_start,
-	[123] = trampoline_prop_chunk,
-	[124] = trampoline_prop_end,
-	[201] = trampoline_map_piodma,
-	[202] = trampoline_unmap_piodma,
-	[206] = iomfbep_cb_match_pmu_service_2,
-	[207] = iomfbep_cb_match_backlight_service,
-	[208] = trampoline_get_time,
-	[211] = trampoline_nop, /* update_backlight_factor_prop */
-	[300] = trampoline_pr_publish,
-	[401] = trampoline_get_uint_prop,
-	[404] = trampoline_nop, /* sr_set_uint_prop */
-	[406] = trampoline_set_fx_prop,
-	[408] = trampoline_get_frequency,
-	[411] = trampoline_map_reg,
-	[413] = trampoline_true, /* sr_set_property_dict */
-	[414] = trampoline_sr_set_property_int,
-	[415] = trampoline_true, /* sr_set_property_bool */
-	[451] = trampoline_allocate_buffer,
-	[452] = trampoline_map_physical,
-	[456] = trampoline_release_mem_desc,
-	[552] = trampoline_true, /* set_property_dict_0 */
-	[561] = trampoline_true, /* set_property_dict */
-	[563] = trampoline_true, /* set_property_int */
-	[565] = trampoline_true, /* set_property_bool */
-	[567] = trampoline_true, /* set_property_str */
-	[574] = trampoline_zero, /* power_up_dart */
-	[576] = trampoline_hotplug,
-	[577] = trampoline_nop, /* powerstate_notify */
-	[582] = trampoline_true, /* create_default_fb_surface */
-	[584] = trampoline_nop, /* IOMobileFramebufferAP::clear_default_surface */
-	[588] = trampoline_nop, /* resize_default_fb_surface_gated */
-	[589] = trampoline_swap_complete,
-	[591] = trampoline_swap_complete_intent_gated,
-	[593] = trampoline_enable_backlight_message_ap_gated,
-	[594] = trampoline_nop, /* IOMobileFramebufferAP::setSystemConsoleMode */
-	[596] = trampoline_false, /* IOMobileFramebufferAP::isDFBAllocated */
-	[597] = trampoline_false, /* IOMobileFramebufferAP::preserveContents */
-	[598] = trampoline_nop, /* find_swap_function_gated */
-};
 
 /*
  * Callback for swap requests. If a swap failed, we'll never get a swap
