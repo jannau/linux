2f0f6fd87826c60ca645c6103f0f331e2d61b6f3
Alba Mendez <me@alba.sh>
2026-07-28 11:28:08 +0200

    spmi: apple: Validate FIFO state
    Check for data before reading the body of a reply, and check for
    end of data afterwards.
    
    Signed-off-by: Alba Mendez <me@alba.sh>
    Signed-off-by: Sasha Finkelstein <k@chaosmail.tech>

--- drivers/spmi/spmi-apple-controller.c
+++ drivers/spmi/spmi-apple-controller.c
@@ -79,6 +79,10 @@ static int spmi_read_cmd(struct spmi_controller *ctrl, u8 opc, u8 sid,
 
 	/* Read SPMI data reply */
 	while (len_read < len) {
+		if (readl(spmi->regs + SPMI_STATUS_REG) & SPMI_RX_FIFO_EMPTY) {
+			dev_err(&ctrl->dev, "FIFO lacks reply data, controller stuck?\n");
+			return -EIO;
+		}
 		rsp = readl(spmi->regs + SPMI_RSP_REG);
 		i = 0;
 		while ((len_read < len) && (i < 4)) {
@@ -86,6 +90,9 @@ static int spmi_read_cmd(struct spmi_controller *ctrl, u8 opc, u8 sid,
 			i += 1;
 		}
 	}
+
+	if (!(readl(spmi->regs + SPMI_STATUS_REG) & SPMI_RX_FIFO_EMPTY))
+		dev_warn(&ctrl->dev, "FIFO has extra data\n");
 
 	return 0;
 }
@@ -115,6 +122,9 @@ static int spmi_write_cmd(struct spmi_controller *ctrl, u8 opc, u8 sid,
 
 	/* Discard */
 	readl(spmi->regs + SPMI_RSP_REG);
+
+	if (!(readl(spmi->regs + SPMI_STATUS_REG) & SPMI_RX_FIFO_EMPTY))
+		dev_warn(&ctrl->dev, "FIFO has extra data\n");
 
 	return 0;
 }
